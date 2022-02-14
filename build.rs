af892918955995f172847212b9118de17913a4c5
Ahmed Riza <dr.riza@gmail.com>
2022-02-14 15:50:29 +0000

    Rust wrapper for Apache hawq libhdfs3

--- build.rs
+++ build.rs
@@ -36,7 +36,8 @@ fn build_hdfs3_lib() {
 
     let dst = cmake::build("libhdfs3");
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static=hdfs3");
+    println!("cargo:rustc-link-lib=hdfs3");
+    // println!("cargo:rustc-link-lib=static=hdfs3");
     // The following are required when linking statically with libhdfs3
     println!("cargo:rustc-link-lib=dylib=stdc++");
     println!("cargo:rustc-link-lib=dylib=protobuf");
