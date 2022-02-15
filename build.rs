2f4b37127d1ff3b7068601f84058a06a99a1bc1c
Ahmed Riza <dr.riza@gmail.com>
2022-02-15 11:47:42 +0000

    Rust wrapper for Apache hawq libhdfs3

--- build.rs
+++ build.rs
@@ -6,28 +6,6 @@ fn main() {
     // build_hdfs3_ffi();
 }
 
-// This is the FFI builder for the Apache Hawq hdfs3 C++ library with its C bindings
-fn build_hdfs3_ffi() {
-    // Tell cargo to invalidate the built crate whenever the wrapper changes.
-    println!("cargo:rerun-if-changed=wrapper.h");
-
-    let bindings = bindgen::Builder::default()
-        // The input headers we would like to generate bindings for
-        .header("wrapper.h")
-        .allowlist_function("nmd.*")
-        .allowlist_function("hdfs.*")
-        .allowlist_function("hadoop.*")
-        .rustified_enum("tObjectKind")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings!");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("hdfs3_bindings.rs"))
-        .expect("Couldn't write bindings!");
-}
-
 fn build_hdfs3_lib() {
     println!(
         "cargo:rerun-if-changed={}",
@@ -53,3 +31,26 @@ fn get_hdfs3_file_path(filename: &'static str) -> String {
 fn get_hdfs3_source_dir() -> &'static str {
     "libhdfs3"
 }
+
+// This is the FFI builder for the Apache Hawq hdfs3 C++ library with its C bindings
+fn build_hdfs3_ffi() {
+    // Tell cargo to invalidate the built crate whenever the wrapper changes.
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        // The input headers we would like to generate bindings for
+        .header("wrapper.h")
+        .allowlist_function("nmd.*")
+        .allowlist_function("hdfs.*")
+        .allowlist_function("hadoop.*")
+        .rustified_enum("tObjectKind")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings!");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("hdfs3_bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
